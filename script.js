5d78b4e2a313cea1f474c506af493521bbe1244b
Patrick <phalloran89@gmail.com>
2023-08-30 16:22:39 +0100

    Add checkDecimal() function that prevents multiple decimal points being used

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let operatorArray = [];
 const buttons = document.querySelectorAll('.calc-button');
 
 function updateDisplay() {
+    displayValue = displayValue.toString();
     const display = document.querySelector('.display');
     if (displayValue.length > 13) {
         display.style = "font-size: 20px";
@@ -21,33 +22,38 @@ updateDisplay();
 function buttonClick() {
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', () => {
-            if (displayValue === '-' || displayValue === '+' || displayValue === '*' || displayValue === '/') {
+            if (displayValue === '-' || displayValue === '+' || 
+            displayValue === '*' || displayValue === '/') {
                 displayValue = '';
             }
-            if (buttons[i].classList.contains('operand')) {
-                inputOperand(buttons[i].value);
+                if (buttons[i].classList.contains('operand')) {
+                    checkDecimal();
+                    inputOperand(buttons[i].value);
+                } else if (buttons[i].classList.contains('operator')) {
+                    inputOperator(buttons[i].value);
+                } else if (buttons[i].classList.contains('equals')) {
+                    operation();
+                } else if (buttons[i].classList.contains('clear')) {
+                    clear();
+                } else if (buttons[i].classList.contains('delete')) {
+                    deleteLast();
+                } else if (buttons[i].classList.contains('negative')) {
+                    switchNegative();
+                }
                 updateDisplay();
-            } else if (buttons[i].classList.contains('operator')) {
-                inputOperator(buttons[i].value);
-                updateDisplay();
-            } else if (buttons[i].classList.contains('equals')) {
-                operation();
-                updateDisplay();
-            } else if (buttons[i].classList.contains('clear')) {
-                clear();
-                updateDisplay();
-            } else if (buttons[i].classList.contains('delete')) {
-                deleteLast();
-                updateDisplay();
-            } else if (buttons[i].classList.contains('negative')) {
-                switchNegative();
-                updateDisplay();
-            }
         });
     }
 };
 
 buttonClick();
+
+function checkDecimal() {
+    if (displayValue.toString().includes('.')) {
+        document.getElementById('decimal').value = '';
+    } else {
+        document.getElementById('decimal').value = '.';
+    }
+};
 
 function switchNegative() {
     if (displayValue.charAt(0) === '-') {
@@ -93,30 +99,32 @@ function operation() {
     if (numberArray.length > 1) {
         for (let i = numberArray.length; i > 1; i--) {
             switch(operatorArray[0]) {
-                case '-': currentTotal = Number(numberArray[0]) - Number(numberArray[1]);
+                case '-': currentTotal = 
+                Number(numberArray[0]) - Number(numberArray[1]);
                     operatorArray.splice(0, 1);
                     numberArray.splice(0, 2);
                     numberArray.unshift(currentTotal);
                     break;
-                case '+': currentTotal = Number(numberArray[0]) + Number(numberArray[1]);
+                case '+': currentTotal = 
+                Number(numberArray[0]) + Number(numberArray[1]);
                     operatorArray.splice(0, 1);
                     numberArray.splice(0, 2);
                     numberArray.unshift(currentTotal);
                     break;
-                case '*': currentTotal = Number(numberArray[0]) * Number(numberArray[1]);
+                case '*': currentTotal = 
+                Number(numberArray[0]) * Number(numberArray[1]);
                     operatorArray.splice(0, 1);
                     numberArray.splice(0, 2);
                     numberArray.unshift(currentTotal);
                     break;
-                case '/': currentTotal = Number(numberArray[0]) / Number(numberArray[1]);
+                case '/': currentTotal = 
+                Number(numberArray[0]) / Number(numberArray[1]);
                     operatorArray.splice(0, 1);
                     numberArray.splice(0, 2);
                     numberArray.unshift(currentTotal);
                     break;
             }
             displayValue = currentTotal;
-            console.log(numberArray);
-            console.log(operatorArray);
             updateDisplay();
         }
         numberArray.length = 0;
